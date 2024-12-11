e32560387c9d1e3d758baf9318508599c377b775
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2024-12-11 16:31:53 +0200

    create evaluate

---
+++ src/evaluate.py
@@ -0,0 +1,27 @@
+from pandas import DataFrame
+
+from data_loader import get_dataframe 
+
+def evaluate(
+        set_name: str, # train/ val
+        results_df: DataFrame
+ ) -> float:
+    """
+    Return MRR@10 score for a prediction
+
+    Parameters:
+        results_df (DataFrame): The predictions. Structure: accommodation_id,user_id,review_1,review_2,review_3,...,review_10
+
+    Returns:
+        float: MRR@10.
+    """
+    matches_df = get_dataframe(set_name, "matches")
+
+    assert len(results_df) == len(matches_df), "results and matches should have the same size."
+    mrr = 0
+
+    for rank, column_name in enumerate(results_df.columns[2: ], 1):
+        column_matches_count = (matches_df.values == results_df[["accommodation_id", "user_id", column_name]]).all(axis=1).sum()
+        mrr += column_matches_count * (1.0 / rank)
+    
+    mrr = mrr / len(matches_df)
