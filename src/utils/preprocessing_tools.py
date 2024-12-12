319f1547dcb6b374cda8d9b30ff400c9da6cee9c
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2024-12-12 19:50:27 +0200

    Update preprocessing_tools.py
    - Fix concatenation func doesn't return the result.
    - make the main a function so can be imported and called independently.

--- src/utils/preprocessing_tools.py
+++ src/utils/preprocessing_tools.py
@@ -9,14 +9,14 @@ def concatenate_data(set_name: str) -> None:
     """
     users_df = csv_to_dataframe(set_name, "users")
     matches_df = csv_to_dataframe(set_name, "matches")
-    
     processed_df = users_df.merge(matches_df, on="user_id")
 
     reviews_df = csv_to_dataframe(set_name, "reviews")
-    
-    processed_df = processed_df.merge(reviews_df, on="accommodation_id")
+    processed_df = processed_df.merge(reviews_df, on="review_id")
 
-if __name__ == "__main__":
+    return processed_df
+
+def concatenate_train_val() -> None:
     processed_train = concatenate_data("train")
     processed_to_csv(processed_train, "train")
     print("train concatenated")
@@ -24,3 +24,6 @@ if __name__ == "__main__":
     processed_val = concatenate_data("val")
     processed_to_csv(processed_val, "val")
     print("val concatenated")
+
+if __name__ == "__main__":
+    concatenate_train_val()
