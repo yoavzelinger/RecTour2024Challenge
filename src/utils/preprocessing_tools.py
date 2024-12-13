f97c5603bbc9346ea2350fb5aad7f758456275d5
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2024-12-13 12:21:22 +0200

    Update preprocessing_tools.py

--- src/utils/preprocessing_tools.py
+++ src/utils/preprocessing_tools.py
@@ -17,26 +17,32 @@ def concatenate_data(set_name: str) -> None:
     # Drop accommodation_id column
     reviews_df = reviews_df.drop(columns="accommodation_id")
     processed_df = processed_df.merge(reviews_df, on="review_id")
-
     return processed_df
 
-def concatenate_train_val() -> None:
-    processed_train = concatenate_data("train")
-    processed_to_csv(processed_train, "train")
-    print("train concatenated")
-    
-    processed_val = concatenate_data("val")
-    processed_to_csv(processed_val, "val")
-    print("val concatenated")
+def create_concatenated_set(set_name: str) -> None:
+    """
+    Concatenate data for a given set_name.
 
-def accommodation_reviews(set_name) -> None:
+    Parameters:
+        set_name (str): The set name. It can be 'train' or 'val'
+    """
+    processed_set = concatenate_data(set_name)
+    processed_to_csv(processed_set, set_name)
+    print(f"{set_name} concatenated and saved")
+    return processed_set
+
+def create_accommodation_reviews(set_name) -> None:
     reviews_df = csv_to_dataframe(set_name, "reviews")
     accommodation_reviews = reviews_df.groupby("accommodation_id").agg({"review_id": " ".join}).reset_index()
     # Convert to dictionary
     accommodation_reviews = accommodation_reviews.to_dict(orient="records")
     accommodation_reviews = {accommodation_review["accommodation_id"]: accommodation_review["review_id"].split() for accommodation_review in accommodation_reviews}
     save_to_pickle(accommodation_reviews, f"{set_name}_reviews_grouped_by_accommodation")
+    print(f"Accommodation reviews for {set_name} set aggregated")
     return accommodation_reviews
     
 if __name__ == "__main__":
-    concatenate_train_val()
+    for set_name in ["train", "val"]:
+        concatenate_set(set_name)
+        accommodation_reviews(set_name)
+    accommodation_reviews("test")
